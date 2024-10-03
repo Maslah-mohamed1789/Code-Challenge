f639fac280f399350e1d2c2c13048c809d238289
Maslah-mohamed1789 <maslahmohamed92@gmail.com>
2024-10-03 15:05:38 +0300

    Update Speed Detector.js

--- Speed Detector.js
+++ Speed Detector.js
@@ -1,12 +1,12 @@
-function speedLimit(speed) {
-    const speedLimit = 70;
-    if (speed < speedLimit) {
-        console.log("Ok");
-    } else {
-        const points = Math.floor((speed - speedLimit) / 5);
-        console.log(`Points: ${points}`);
-        if (points > 12) {
-            console.log("License suspended");
-        }
-    }
-}
+// function speedLimit(speed) {
+//     const speedLimit = 70;
+//     if (speed < speedLimit) {
+//         console.log("Ok");
+//     } else {
+//         const points = Math.floor((speed - speedLimit) / 5);
+//         console.log(`Points: ${points}`);
+//         if (points > 12) {
+//             console.log("License suspended");
+//         }
+//     }
+// }
