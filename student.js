54d781486fb8d63fa669272a8a08ef97846c37ec
Maslah-mohamed1789 <maslahmohamed92@gmail.com>
2024-10-03 15:04:36 +0300

    Update student.js

--- student.js
+++ student.js
@@ -1,21 +1,21 @@
-function Grade(marks){
-    if(marks >= 80 && marks <= 100) {
-        return 'A';
+// function Grade(marks){
+//     if(marks >= 80 && marks <= 100) {
+//         return 'A';
 
-    }  else if (marks >= 60 && marks < 80) {
-        return 'B';
+//     }  else if (marks >= 60 && marks < 80) {
+//         return 'B';
 
-    } else if (marks >= 49 && marks < 60) {
-        return 'C';
+//     } else if (marks >= 49 && marks < 60) {
+//         return 'C';
 
-    } else if (marks >= 40 && marks < 49) {
-        return 'D';
+//     } else if (marks >= 40 && marks < 49) {
+//         return 'D';
 
-    } else if (marks < 40 && marks >= 0) {
-        return 'E';
+//     } else if (marks < 40 && marks >= 0) {
+//         return 'E';
         
-    } else {
-        return 'Invalid marks. Please enter a value between 0 and 100.';
-    }
+//     } else {
+//         return 'Invalid marks. Please enter a value between 0 and 100.';
+//     }
 
-}
+// }
